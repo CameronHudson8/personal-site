53df4a557f46ad36cec56a875c1329fd299ec54b
Cameron Hudson <CameronHudson8@gmail.com>
2020-09-15 19:39:40 -0700

    feat: pull only the relevant files into frontend/

--- backend/laravel/public/js/template.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-
-  // Construct footer.
-  var footerContent = str($('footer').append("<img src="images/comodo_secure_seal_113x59_transp.png" alt="Comodo Secure">")";
-  document.getElementsByTagName('head')[0].appendChild( ... )
-
-};
